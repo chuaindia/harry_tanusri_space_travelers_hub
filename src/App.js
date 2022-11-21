2eff7405ede5191a9123829d4c4ad1b7f45b6a9b
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-21 23:54:32 +0530

    Add files and folders for myprofile setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import Header from '.components/header/header';
+import Mision from '.components/Pages/Mission';
+import Profile from '.components/Pages/Profile';
+import Rocket from '.components/Pages/Rocket';
 import './App.css';
+import Mission from './components/Pages/Mission';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rocket/>} />
+        <Route path="/missions" element={<Mission/>} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+    </div>  
   );
 }
 
---
+++ src/components/Pages/Rocket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rocket() {
+  return (
+    <div>
+      <h2>Rocket&lsquo;s Page</h2>
+    </div>
+  );
+}
+
+export default Rocket;
