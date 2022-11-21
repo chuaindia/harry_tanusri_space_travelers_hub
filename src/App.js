c7b77f79174670c2f519c89635dfd7aa1cc8467f
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-21 23:38:19 +0530

    Add files and folders for Header Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import Header from '.components/header/header';
 import './App.css';
 
 function App() {
---
+++ src/components/header/header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './header.css';
+import logo from '../../image/logo.png';
+
+const Header = () => {
+  const links = [
+    {
+      id: 1,
+      text: 'Rocket',
+      path: '/',
+    },
+    {
+      id: 2,
+      text: 'Mission',
+      path: '/missions',
+    },
+    {
+      id: 3,
+      text: 'My profile',
+      path: '/profile',
+    },
+  ];
+
+  const navList = links.map((link) => (
+    <li key={link.id} className="navlink">
+      <NavLink className="navLink" to={link.path}>{link.text}</NavLink>
+    </li>
+  ));
+
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <img className="navLogo" src={logo} alt="Logo" />
+          <a className="navbarBrand" href="/">Space Traveler&lsquo;s Hub</a>
+          <button className="navbarToggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbarTogglerIcon" />
+          </button>
+          <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+            <ul className="navbarnav">
+              {navList}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
