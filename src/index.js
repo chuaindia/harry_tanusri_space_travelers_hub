32a5151e72e458b19228dd5f920dbe563bc174b4
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-22 10:04:27 +0530

    Add files and folders for profile setup

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowseRouter>
+    <BrowserRouter>
       <App />
-    </BrowseRouter>    
+    </BrowserRouter>    
   </React.StrictMode>
 );
 
