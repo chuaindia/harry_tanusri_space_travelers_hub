f061e066cc9559f2ae2ce160e2ff016bf42491eb
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-23 22:39:16 +0530

    Added RocketAPI

--- src/navigationPages/profile/profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Profile = () => (
-  <div>
-    Profile component
-  </div>
-);
-
-export default Profile;
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rocketsReducer from './rockets/rockets';
-
-export default configureStore({
-  reducer: {
-
-    rocket: rocketsReducer,
-  },
-});
