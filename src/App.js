8f63d9e78ae0752d61cd96090f7a6a9224b987cc
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-23 22:40:11 +0530

    Added rocketAPI

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Profile from './navigationPages/profile/profile';
+import Rockets from './components/Rockets';
+// import Mission from './navigationPages/mission/missions';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      {/* <Route path="/mission" element={<Mission />} /> */}
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
