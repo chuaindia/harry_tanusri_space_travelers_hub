49c7160fbab0c125c9f467ffb5886d31c84aa439
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-25 16:12:02 +0530

    Add styling

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './mission/mission.redux';
 import rocketsReducer from './rockets/rockets';
 
+// Reducers
+
 export default configureStore({
   reducer: {
     mission: missionReducer,
