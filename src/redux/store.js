f1151beafcf8fe23980bab9616282ffccd30a695
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-11-25 16:17:02 +0530

    Added style properly

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './mission/mission.redux';
-import rocketsReducer from './rockets/rockets';
+import rocketReducer from './rockets/rockets';
 
 // Reducers
 
 export default configureStore({
   reducer: {
     mission: missionReducer,
-    rocket: rocketsReducer,
+    rocket: rocketReducer,
   },
 });
